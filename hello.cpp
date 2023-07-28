d4dc29c4c5d9fe0dd3cdce9b43a09b42a836511c
Nnanna <omokennanna832@gmail.com>
2023-07-28 16:23:21 +0100

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    cout << "Hello There\nMy name is Nnanna Omoke\nWelcome to my learning repository\n";
+}
